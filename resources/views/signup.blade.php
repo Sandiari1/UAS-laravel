cb2512383cd42053a6877360fac65365b420baa7
Sandiari1 <sandiarimuzaki@gmail.com>
2024-07-12 18:48:09 +0700

    hahah

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base-auth')
 
 @section('asset-image')
-<img src="{{asset('Assets/Img/employes-bureauv.jpg')}}" class="login-img" />
+<img src="{{asset('Assets/Img/walling-XLqiL-rz4V8-unsplash.jpg')}}" class="login-img" />
 @endsection
 
 @section('title', 'Sign Up')
